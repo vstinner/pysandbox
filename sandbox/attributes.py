f86886d046cd1bf31753d64a23cd3c7a450dc8af
Victor Stinner <victor.stinner@haypocalc.com>
2010-02-02 03:16:08 +0100

    fix HideAttributes for Python 2.6+

--- sandbox/attributes.py
+++ sandbox/attributes.py
@@ -1,4 +1,9 @@
 from types import FunctionType
+try:
+    from sys import _clear_type_cache
+except ImportError:
+    def _clear_type_cache():
+        pass
 
 from .cpython import dictionary_of
 from .restorable_dict import RestorableDict
@@ -17,4 +22,6 @@ class HideAttributes:
 
     def disable(self, sandbox):
         self.function_dict.restore()
+        # Python 2.6+ uses a method cache: clear it to avoid errors
+        _clear_type_cache()
 
