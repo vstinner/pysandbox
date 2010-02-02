f12596d05495c57527bd1cd7fd99db70366bdba9
Victor Stinner <victor.stinner@haypocalc.com>
2010-02-02 02:16:19 +0100

    create interpreter.py

---
+++ interpreter.py
@@ -0,0 +1,8 @@
+from __future__ import with_statement
+from code import InteractiveConsole
+from sandbox import Sandbox
+import readline
+
+with Sandbox():
+    InteractiveConsole().interact("Try to break the sandbox!")
+
