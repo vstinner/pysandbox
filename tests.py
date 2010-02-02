3fc838d3580426b76f47d1d9ff36ebd85ae4f050
Victor Stinner <victor.stinner@haypocalc.com>
2010-02-02 01:16:49 +0100

    test_closure() ensures the func_closure is available again outside the sandbox

--- tests.py
+++ tests.py
@@ -26,27 +26,33 @@ def test_builtin_open():
     with open(filename) as fp:
         fp.read()
 
-def test_closure():
-    """
-    Check that a function closure value can not be retrieved.
-    """
+def read_closure_secret():
     def get_cell_value(cell):
         # http://code.activestate.com/recipes/439096/
         return type(lambda: 0)((lambda x: lambda: x)(0).func_code, {}, None, None, (cell,))()
 
+    def closure(secret=42):
+        def _closure():
+            return secret
+        return _closure
+    func = closure()
+    del closure
+    cell = func.func_closure[0]
+    secret = get_cell_value(cell)
+    assert secret == 42
+
+def test_closure():
+    """
+    Check that a function closure value can not be retrieved.
+    """
     with Sandbox():
-        def closure(secret=42):
-            def _closure():
-                return secret
-            return _closure
-        func = closure()
-        del closure
         try:
-            cell = func.func_closure[0]
-            secret = get_cell_value(cell)
+            read_closure_secret()
         except AttributeError, err:
             assert str(err) == "'function' object has no attribute 'func_closure'"
         else:
-            assert secret == 42
-            assert False, "func_closure attribute is present"
+            assert False, "func_closure is present"
+
+    # closure are readable outside the sandbox
+    read_closure_secret()
 
