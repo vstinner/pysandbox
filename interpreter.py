6722778964353f5a27d9df3345909b1e240578d9
Victor Stinner <victor.stinner@haypocalc.com>
2010-02-13 00:41:58 +0100

    interpreter.py: convert the code to a class

--- interpreter.py
+++ interpreter.py
@@ -4,33 +4,42 @@ from sandbox import Sandbox, SandboxConfig
 import readline
 from optparse import OptionParser
 
-def createConfig():
-    config = SandboxConfig('interpreter')
+class SandboxedInterpeter:
+    def __init__(self):
+        self.config, self.options = self.parseOptions()
 
-    parser = OptionParser(usage="%prog [options]")
-    config.createOptparseOptions(parser)
-    parser.add_option("--verbose", "-v", help="Verbose mode",
-        action="store_true", default=False)
-    options, argv = parser.parse_args()
-    if argv:
-        parser.print_help()
-        exit(1)
+        self.config.enable('interpreter')
+        if 'traceback' in self.config.features:
+            self.config.allowPath(__file__)
 
-    config.useOptparseOptions(options)
-    if 'traceback' in config.features:
-        config.allowPath(__file__)
+    def parseOptions(self):
+        config = SandboxConfig()
 
-    if options.verbose:
-        from pprint import pprint
-        print "Sandbox config:"
-        pprint(config.__dict__)
-    else:
-        print "Enabled features: %s" % ', '.join(sorted(config.features))
-    print
+        parser = OptionParser(usage="%prog [options]")
+        config.createOptparseOptions(parser)
+        parser.add_option("--verbose", "-v", help="Verbose mode",
+            action="store_true", default=False)
+        options, argv = parser.parse_args()
+        if argv:
+            parser.print_help()
+            exit(1)
 
-    return config
+        config.useOptparseOptions(options)
+        return config, options
 
-config = createConfig()
-with Sandbox(config):
-    InteractiveConsole().interact("Try to break the sandbox!")
+    def dumpConfig(self):
+        if self.options.verbose:
+            from pprint import pprint
+            print "Sandbox config:"
+            pprint(self.config.__dict__)
+        else:
+            print "Enabled features: %s" % ', '.join(sorted(self.config.features))
+        print
 
+    def main(self):
+        self.dumpConfig()
+        with Sandbox(self.config):
+            InteractiveConsole().interact("Try to break the sandbox!")
+
+if __name__ == "__main__":
+    SandboxedInterpeter().main()
