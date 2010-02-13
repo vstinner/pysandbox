8de1ab6e3ef501c3052286f5ebb337ed6880ef1d
Victor Stinner <victor.stinner@haypocalc.com>
2010-02-13 04:05:54 +0100

    Fix Sandbox.__exit__(): disable protections in the reverse order

--- sandbox/sandbox_class.py
+++ sandbox/sandbox_class.py
@@ -8,13 +8,13 @@ class Sandbox:
             self.config = config
         else:
             self.config = SandboxConfig()
-        self.protections = [protection() for protection in self.PROTECTIONS] 
+        self.protections = [protection() for protection in self.PROTECTIONS]
 
     def __enter__(self):
         for protection in self.protections:
             protection.enable(self)
 
     def __exit__(self, type, value, traceback):
-        for protection in self.protections:
+        for protection in reversed(self.protections):
             protection.disable(self)
 
