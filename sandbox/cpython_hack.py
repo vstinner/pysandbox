c8e1eae76efc13b1a8beffadf11ce6c439114d49
Victor Stinner <victor.stinner@haypocalc.com>
2010-02-15 13:15:48 +0100

    cpython_hack: create set_interp_builtins()
     * Rename struct_typeobject to PyTypeObject
     * Create PyInterpreterState and PyThreadState
     * Create cptr_dump() and cobject_setattr()

--- sandbox/cpython_hack.py
+++ sandbox/cpython_hack.py
@@ -35,15 +35,18 @@ class PyObject_VAR_HEAD(Structure):
 
 destructor = CFUNCTYPE(None, c_void_p)
 
-class struct_typeobject(Structure):
+class PyTypeObject(Structure):
     _fields_ = PyObject_VAR_HEAD._fields_ + (
 	("tp_name", c_char_p),
         ("tp_basicsize", Py_ssize_t),
 	("tp_itemsize", Py_ssize_t),
 	("tp_dealloc", destructor),
-        # ... we don't need more
+        # ... (we don't need to know more to hack CPython)
     )
-struct_typeobject_p = POINTER(struct_typeobject)
+
+    def __repr__(self):
+        return "<type %s>" % self.tp_name
+PyTypeObject_p = POINTER(PyTypeObject)
 
 class PyTryBlock(Structure):
     _fields_ = (
@@ -51,6 +54,27 @@ class PyTryBlock(Structure):
         ("b_handler", c_int),
         ("b_level", c_int),
     )
+
+class PyInterpreterState(Structure):
+    # Python 2.6 attributes
+    _fields_ = (
+        ("next", c_void_p),
+        ("tstate_head", c_void_p),
+        ("modules", PyObject_p),
+        ("sysdict", PyObject_p),
+        ("builtins", PyObject_p),
+        # ... (we don't need to know more to hack CPython)
+    )
+PyInterpreterState_p = POINTER(PyInterpreterState)
+
+class PyThreadState(Structure):
+    # Python 2.6 attributes
+    _fields_ = (
+        ("next", c_void_p),
+        ("interp", PyInterpreterState_p),
+        # ... (we don't need to know more to hack CPython)
+    )
+PyThreadState_p = POINTER(PyThreadState)
 
 class PyFrameObject(Structure):
     # Python 2.6 attributes
@@ -66,7 +90,7 @@ class PyFrameObject(Structure):
         ("f_exc_type", PyObject_p),
         ("f_exc_value", PyObject_p),
         ("f_exc_traceback", PyObject_p),
-        ("f_tstate", c_void_p),
+        ("f_tstate", PyThreadState_p),
         ("f_lasti", c_int),
         ("f_lineno", c_int),
         ("f_iblock", c_int),
@@ -83,9 +107,22 @@ def cptr_at(addresss, type=None):
         addresss = c_void_p(addresss)
     return cast(addresss, type)
 
+_PyObject_Dump = pythonapi._PyObject_Dump
+
+def cptr_dump(cptr):
+    _PyObject_Dump(cptr)
+
 def cobject_at(address, type=None):
     cobj_ptr = cptr_at(address, type)
     return cobj_ptr.contents
+
+def cobject_setattr(cobj, name, value):
+    cvalue = pyobject_get_cobject(value)
+    old_value = getattr(cobj, name)
+    old_value_type = Py_TYPE(old_value).contents
+    Py_DECREF(old_value)
+    Py_INCREF(cvalue)
+    setattr(cobj, name, pyobject_get_cptr(value))
 
 def pyobject_address(obj):
     return id(obj)
@@ -104,10 +141,10 @@ if COUNT_ALLOCS:
 
 def Py_TYPE(cobj_ptr):
     """
-    Get op->ob_type as a struct_typeobject_p.
+    Get op->ob_type as a PyTypeObject_p.
     """
     cobj = cobj_ptr.contents
-    return cast(cobj.ob_type, struct_typeobject_p)
+    return cast(cobj.ob_type, PyTypeObject_p)
 
 if Py_TRACE_REFS:
     _Py_Dealloc = pythonapi._Py_Dealloc
@@ -116,7 +153,7 @@ else:
         type_address = Py_TYPE(cobj_ptr)
         if COUNT_ALLOCS:
             dec_count(type_address)
-        cobj_type = cobject_at(type_address, struct_typeobject)
+        cobj_type = cobject_at(type_address, PyTypeObject)
         cobj_type.tp_dealloc(cobj_ptr)
 
 def Py_DECREF(cobj_ptr):
@@ -129,12 +166,16 @@ def Py_INCREF(obj):
     obj.ob_refcnt += 1
 
 def set_frame_builtins(frame, builtins):
+    # frame.f_buitins = builtins
     cframe = pyobject_get_cobject(frame, PyFrameObject)
-    Py_DECREF(cframe.f_builtins)
+    cobject_setattr(cframe, "f_builtins", builtins)
 
-    cbuiltins = pyobject_get_cobject(builtins)
-    Py_INCREF(cbuiltins)
-    cframe.f_builtins = pyobject_get_cptr(builtins)
+def set_interp_builtins(frame, builtins):
+    # frame.f_tstate.interp.builtins = builtins
+    cframe = pyobject_get_cobject(frame, PyFrameObject)
+    ctstate = cframe.f_tstate.contents
+    cinterp = ctstate.interp.contents
+    cobject_setattr(cinterp, "builtins", builtins)
 
 class ClearFrameCache:
     def __init__(self, frame):
