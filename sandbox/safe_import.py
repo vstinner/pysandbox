28e84c1499faaa127d1dfed52f8891796cc598c6
Victor Stinner <haypo@maggie.(none)>
2010-02-16 03:14:36 +0100

    Deny modification of a SafeImport

--- sandbox/safe_import.py
+++ sandbox/safe_import.py
@@ -1,26 +1,33 @@
-from .proxy import proxy
+from .proxy import proxy, readOnlyError
 
 class SafeModule(object):
-    def __init__(self, module):
-        self.__name = repr(module.__name__)
+    def __init__(self, module, attributes):
+        _set = object.__setattr__
+        name_repr = repr(module.__name__)
         try:
-            self.__file = module.__file__
-            self.__name += " from %r" % self.__file__
+            _set(self, '__file',  module.__file__)
+            name_repr += " from %r" % self.__file__
         except AttributeError:
-            self.__name += " (built-in)"
-        self.__name__ = module.__name__
-        self.__doc__ = module.__doc__
+            name_repr += " (built-in)"
+        _set(self, '__name_repr',  name_repr)
+        _set(self, '__name__', module.__name__)
+        _set(self, '__doc__', module.__doc__)
+        for key, value in attributes.iteritems():
+            _set(self, key, value)
+
+    def __setattr__(self, name, value):
+        readOnlyError()
 
     def __repr__(self):
-        return "<SafeModule %s>" % (self.__name,)
+        return "<SafeModule %s>" % (self.__name_repr,)
 
 def createSafeModule(module, attributes):
-    safe = SafeModule(module)
+    safe_attributes = {}
     for attr in attributes:
         value = getattr(module, attr)
         value = proxy(value)
-        setattr(safe, attr, value)
-    return safe
+        safe_attributes[attr] = value
+    return SafeModule(module, safe_attributes)
 
 def _safe_import(__import__, module_whitelist):
     """
