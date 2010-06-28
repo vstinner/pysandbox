ffbea9026be8a4099c0ba75960b536550814552b
Victor Stinner <haypo@maggie.(none)>
2010-06-28 20:40:08 +0200

    write setup.py script

---
+++ sandbox/version.py
@@ -0,0 +1,4 @@
+PACKAGE = "sandbox"
+VERSION = "0.0"
+LICENSE = "BSD"
+URL = "http://github.com/haypo/pysandbox/"
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from distutils.core import setup, Extension
+import imp
+
+version = imp.load_source('version', 'sandbox/version.py')
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Natural Language :: English',
+    'Programming Language :: C',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
+
+options = {
+    'name': version.PACKAGE,
+    'version': version.VERSION,
+    'license': version.LICENSE,
+    'url': version.URL,
+    'ext_modules': [Extension('_sandbox', ['_sandbox/module.c'])],
+    'classifiers': CLASSIFIERS,
+    'packages': ('sandbox',),
+}
+
+setup(**options)
+
