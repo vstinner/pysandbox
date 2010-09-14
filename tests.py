87f365e86320f38c087326b7efeca3fea03c402f
Victor Stinner <victor.stinner@haypocalc.com>
2010-09-15 01:38:52 +0200

    fix test_interpreter() for Python3

--- tests.py
+++ tests.py
@@ -881,18 +881,17 @@ def get_interpreter_stdout(*code, **kw):
             % (process.returncode, stdout))
     assert process.returncode == 0
     stdout = stdout.splitlines()
-    assert stdout[0] == '', stdout[0]
-    assert stdout[1] == '', stdout[1]
-    stdout = stdout[2:]
-    return stdout
+    assert stdout[0] == bytes_literal(''), stdout[0]
+    assert stdout[1] == bytes_literal(''), stdout[1]
+    return stdout[2:]
 
 def test_interpreter():
     stdout = get_interpreter_stdout('1+1')
-    assert stdout == [r"sandbox>>> 2", 'sandbox>>> ']
+    assert stdout == [bytes_literal(r"sandbox>>> 2"), bytes_literal('sandbox>>> ')]
 
     for encoding in ('latin_1', 'utf_8'):
         stdout = get_interpreter_stdout(u'u"\xe9"', encoding=encoding)
-        assert stdout == [r"sandbox>>> u'\xe9'", 'sandbox>>> ']
+        assert stdout == [bytes_literal(r"sandbox>>> u'\xe9'"), bytes_literal('sandbox>>> ')]
 
 def parseOptions():
     from optparse import OptionParser
