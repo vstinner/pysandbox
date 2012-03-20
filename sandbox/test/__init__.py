1daa653155ef1f6bb4465d5a1f1d670109d1c8ea
Victor Stinner <victor.stinner@gmail.com>
2012-03-20 23:48:00 +0100

    Tests: Fix createSandboxConfig()
    Add the missing use_subprocess variable

--- sandbox/test/__init__.py
+++ sandbox/test/__init__.py
@@ -13,9 +13,13 @@ def createSandboxConfig(*features, **kw):
     if (createSandboxConfig.cpython_restricted is not None) \
     and ('cpython_restricted' not in kw):
         kw['cpython_restricted'] = createSandboxConfig.cpython_restricted
+    if (createSandboxConfig.use_subprocess is not None) \
+    and ('use_subprocess' not in kw):
+        kw['use_subprocess'] = createSandboxConfig.use_subprocess
     return SandboxConfig(*features, **kw)
 createSandboxConfig.debug = False
 createSandboxConfig.cpython_restricted = None
+createSandboxConfig.use_subprocess = None
 
 def createSandbox(*features):
     config = createSandboxConfig(*features)
